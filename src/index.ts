12c0da9bbca2c666a543f87e75948f9fa2987561
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-05-15 09:27:36 +0000

    refactor: use private modifiers

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 const addon = require('../native')
 
 export class AhoCorasick {
-  #instance: unknown
+  private instance: unknown
 
   constructor(
     patterns: string[]
   , options: { caseSensitive: boolean }
   ) {
-    this.#instance = addon.createAhoCorasick(patterns, options)
+    this.instance = addon.createAhoCorasick(patterns, options)
   }
 
   isMatch(text: string): boolean {
-    return addon.isMatch(this.#instance, text)
+    return addon.isMatch(this.instance, text)
   }
 
   findAll(text: string): string[] {
-    return addon.findAll(this.#instance, text)
+    return addon.findAll(this.instance, text)
   }
 }
