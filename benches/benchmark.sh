17f9c7226e26b3b1d4019b1ef817c9c415e72ff6
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-07-09 03:17:57 +0000

    refactor: rename /benchmark to /benches

--- benches/benchmark.sh
+++ benches/benchmark.sh
@@ -4,18 +4,18 @@ yarn tsc \
   --module commonjs \
   --target esnext \
   --esModuleInterop \
-  ./benchmark/*.ts
+  ./benches/*.ts
 
 echo '======================='
 echo 'benchmark: aho-corasick'
 echo '======================='
-node ./benchmark/benchmark-aho-corasick.js
+node ./benches/benchmark-aho-corasick.js
 
 echo ''
 
 echo '==================='
 echo 'benchmark: fastscan'
 echo '==================='
-node ./benchmark/benchmark-fastscan.js
+node ./benches/benchmark-fastscan.js
 
-rm ./benchmark/*.js
+rm ./benches/*.js
